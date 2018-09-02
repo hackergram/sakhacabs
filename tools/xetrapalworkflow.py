e01b1dd2de80682f1df8ced11bedb0f5ef86dc31
Arjun Venkatraman <arjun@mojolab.org>
2018-09-02 22:43:12 +0530

    Updated

--- tools/xetrapalworkflow.py
+++ tools/xetrapalworkflow.py
@@ -4,4 +4,4 @@ import xetrapal
 sys.path.append("/opt/mojomailman/mojomail")
 from mojomailGMail import *
 sakhacabsxpal=xetrapal.Xetrapal(configfile="/home/arjun/sakhacabs/sakhacabsxpal.conf")
-sakhamailer=sakhacabsxpal.get_mojomail()
+sakhamailer=sakhacabsxpal.get_mojogmail()
