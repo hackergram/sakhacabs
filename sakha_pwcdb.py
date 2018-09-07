3d3324de71d8f3b241dd397153b93148c5f5d809
karan100010 <karanjoshi1993@gmail.com>
2018-09-08 01:06:00 +0530

    first schema added

--- sakha_pwcdb.py
+++ sakha_pwcdb.py
@@ -1,13 +1,21 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep  7 11:33:20 2018
+
+@author: lilhack110
+"""
+
 import pygsheets
 gd=pygsheets.authorize("/home/lilhack110/authdir/sheets.googleapis.com-python.json")
-sakha=gd.open_by_key("1tcZWw19HBLta8Dmnby9ScjZC5S0fhYumfsIfDW6r3e8")
-sakha_ws=sakha.worksheet_by_title("2018")
+sakha=gd.open_by_key("1a5U8WAN6PclcGCGMyhKENDVkZ3LegNkyjEZTWPBI5NQ")
+sakha_ws=sakha.worksheet_by_title("Sheet1")
 sakhadf=sakha_ws.get_as_df()
 sakhadf.columns
 
 #sakhadf.drop(sakhadf.ix[:,'Duty Cancelled':'No o f Ride'].head(0).columns, axis=1)
 #sakha_cabs_df_drop=sakhadf.drop(sakhadf.ix[:,'Duty Cancelled':'No o f Ride'].head(0).columns, axis=1)
-gd.list_ssheets()
+#gd.list_ssheets()
 sakha=gd.open_by_key("1QDzE009LaHhNbcF-xy5Fo1Kjj2Lj_u7iayITphcH1T4")
 sakha_ws=sakha.worksheet_by_title("2018")
 sakhadf=sakha_ws.get_as_df()
@@ -19,12 +27,8 @@ sakhadf=sakha_ws.get_as_df()
 import pymongo
 client= pymongo.MongoClient("mongodb://localhost:27017/")
 sakha1=client.sakha
-roster1=sakha1.mis_roster
+roster1=sakha1.mis_roster_pwc1
 #client.list_database_names
-#print(client.list_database_names())
-#sakha1.insert_many(df.to_dict('records'))
-#sakha1.insert_many(sakhacdfdf.to_dict('records'))
-#sakha1.insert_many(sakhacdf.to_dict('records'))
-roster1=sakha1.mis_roster
-#roster1.insert_many(sakhacdfdf.to_dict('records'))
+print(client.list_database_names())
+
 roster1.insert_many(sakhadf.to_dict('records'))
