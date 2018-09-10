58012823b222c224a9bfeebfe8d0a9042ce3f8f4
Arjun Venkatraman <arjun@mojolab.org>
2018-09-10 07:49:33 +0530

    Updated

--- lib/sakhacabsfunctions.py
+++ lib/sakhacabsfunctions.py
@@ -6,15 +6,14 @@ Created on Sat Sep  8 21:52:07 2018
 @author: arjun
 """
 
-import sys,os
+import sys,os,json
 from uuid import uuid4
 import datetime
 sys.path.append("/opt/xetrapal")
 from sakhacabsdatamodel import User,LocationUpdate,DutySlip,Vehicle
 import xetrapal
 from couchdbkit import *
-server=Server()
-db=server['sakhacabs']
+
 def new_user(telegram_id,role,logger=xetrapal.astra.baselogger,**kwargs):    
     meta={}
     for key in kwargs.keys():
@@ -35,24 +34,42 @@ def new_vehicle(vehicle_num,logger=xetrapal.astra.baselogger,**kwargs):
 
 
 
-def new_dutyslip(driver_id,vehicle_id,logger=xetrapal.astra.baselogger,**kwargs): 
+def new_dutyslip(driver,vehicle,logger=xetrapal.astra.baselogger,**kwargs): 
     created_time=datetime.datetime.utcnow()
-    dutyslip=DutySlip(dutyslip_id=str(uuid4()),created_time=created_time,driver_id=driver_id)
+    dutyslip=DutySlip(dutyslip_id=str(uuid4()),created_time=created_time,driver_id=driver._id,vehicle_id=vehicle._id)
     logger.info(u"New dutyslip with id {} created".format(dutyslip.dutyslip_id))
     return dutyslip
 
-def new_locationupdate(driver_id,location,timestamp,checkin=True,logger=xetrapal.astra.baselogger,**kwargs): 
-    locationupdate=LocationUpdate(driver_id=driver_id,timestamp=timestamp,location=location,checkin=checkin)
+def new_locationupdate(driver,location,timestamp,checkin=True,logger=xetrapal.astra.baselogger,**kwargs): 
+    locationupdate=LocationUpdate(driver_id=driver._id,timestamp=timestamp,location=location,checkin=checkin)
     if checkin==True:
         logger.info(u"New checkin from driver with id {} at {} from {}".format(locationupdate.driver_id,locationupdate.timestamp,locationupdate.location))
     else:
         logger.info(u"Checkout from driver with id {} at {} from {}".format(locationupdate.driver_id,locationupdate.timestamp,locationupdate.location))
     return locationupdate
 
+def get_driver_by_tgid(tgid):
+    t=db.view("user/driver_by_telegram",keys=[tgid]).all()
+    if len(t)>0:
+        return[User(x['value']) for x in t][0]
+    else:
+        return None
+def get_customer_by_tgid(tgid):
+    t=db.view("user/cust_by_telegram",keys=[tgid]).all()
+    if len(t)>0:
+        return [User(x['value']) for x in t][0]
+    else:
+        return None
 
-
-
-def textmessagehandler(bot,update,logger=xetrapal.astra.baselogger):
+def messagehandler(bot,update,logger=xetrapal.astra.baselogger,xpalbot=None):
+    if xpalbot!=None:
+        logger=xpalbot.logger
+        uids = [user['id'] for user in xpalbot.users]
+        if update.message.from_user.id not in uids:
+            logger.info("Adding user to user list")
+            xpalbot.users.append(json.loads(update.message.from_user.to_json()))
+        else:
+            logger.info("User in list already")
     try:
         logger.info(u"Got message  {} from user {}".format(update.message.text,update.message.from_user.id))
     except Exception as e:
--- tools/xetrapalworkflow.py
+++ tools/xetrapalworkflow.py
@@ -3,5 +3,17 @@ sys.path.append("/opt/xetrapal")
 import xetrapal
 sys.path.append("/opt/mojomailman/mojomail")
 from mojomailGMail import *
+sys.path.append("/opt/sakhacabs/lib")
+from sakhacabsfunctions import *
 sakhacabsxpal=xetrapal.Xetrapal(configfile="/home/arjun/sakhacabs/sakhacabsxpal.conf")
 sakhamailer=sakhacabsxpal.get_mojogmail()
+driverbotconfig=xetrapal.karma.load_config(configfile="/home/arjun/sakhacabs/driversakhabot.conf")
+sakhahelperconfig=xetrapal.karma.load_config(configfile="/home/arjun/sakhacabs/sakhahelper.conf")
+
+driversakhabot=xetrapal.telegramastras.XetrapalTelegramBot(config=driverbotconfig,logger=sakhacabsxpal.logger)
+sakhahelper=xetrapal.telegramastras.XetrapalTelegramBot(config=sakhahelperconfig,logger=sakhacabsxpal.logger)
+
+server=Server()
+db=server['sakhacabs']
+
+User.set_db(db)
