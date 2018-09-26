104567f87bb114862aec12ea5dc58203a5512ad2
Arjun Venkatraman <arjun@mojolab.org>
2018-09-26 16:26:39 +0530

    Added API functionality for UI purposes

--- tools/dispatcher.py
+++ tools/dispatcher.py
@@ -5,5 +5,44 @@ Created on Sat Sep 22 20:54:42 2018
 
 @author: arjun
 """
+from driversakhabotmongo import *
 from flask import Flask
 app = Flask(__name__)
+
+@app.route("/user/all")
+def api_get_users():
+    return User.objects.to_json()
+
+@app.route("/user/by_tgid/<tgid>")
+def api_get_user_by_tgid(tgid):
+    return None
+@app.route("/driver/all")
+def api_get_drivers():
+    return User.objects(role="driver").to_json()
+
+@app.route("/driver/by_tgid/<tgid>")
+def api_get_driver_by_tgid(tgid):
+    return None
+@app.route("/vehicle/all")
+def api_get_vehicles():
+    return Vehicle.objects.to_json()
+
+@app.route("/vehicle/by_vnum/<vnum>")
+def api_get_vehicle_by_vnum(vnum):
+    return None
+
+@app.route("/locupdate/all")
+def api_get_locupdates():
+    return LocationUpdate.objects.to_json()
+
+@app.route("/locupdate/by_driver/<tgid>")
+def api_get_locupdates_for_driver(tgid):
+    return None
+    
+
+
+
+
+
+
+
